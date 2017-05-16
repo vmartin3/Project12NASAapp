ed45f4ddd0ef1ab909f61b4ec45ddc6e27a0fac3
vmartin3 <vernonmartin11@gmail.com>
2017-05-16 13:14:00 -0400

    Initial Commit

---
+++ Proj12NASAappTests/Proj12NASAappTests.swift
@@ -0,0 +1,36 @@
+//
+//  Proj12NASAappTests.swift
+//  Proj12NASAappTests
+//
+//  Created by Vernon G Martin on 5/16/17.
+//  Copyright © 2017 Vernon G Martin. All rights reserved.
+//
+
+import XCTest
+@testable import Proj12NASAapp
+
+class Proj12NASAappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
